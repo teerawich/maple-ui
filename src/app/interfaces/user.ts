e998ee8c5714d310328a970914ea3aa884cd4a58
Teerawich <teerawich@giinos.com>
2024-08-18 23:20:00 +0700

    [update] FW006 page update status.

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,8 +1,12 @@
+import { Accomplice } from './accomplice';
+
 export interface User {
+  accomplice?: Accomplice;
   createdAt: Date;
   email: string;
   id: string;
   isActive: boolean;
   name: string;
+  phone: string;
   updatedAt: Date;
 }
